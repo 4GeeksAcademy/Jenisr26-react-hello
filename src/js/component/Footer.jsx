73254ec6a1699348ee1b10042f7341c1841f839d
Jenny <jenisaro07@gmail.com>
2025-01-15 18:55:54 +0000

    bjh

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";//1
+
+
+export const Aside = () => {
+    console.log();
+    
+}
+
+//5
+//2
+export const Footer = () => {
+    //3Code JS
+    
+    //4 Return un elemento HTML
+    return (
+        <h1>Soy el Footer</h1>
+    )
+
+}
+
+
+//6 y 7 
